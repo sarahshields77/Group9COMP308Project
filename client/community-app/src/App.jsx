25d07f42a89efc7d4b96d6551f0c2eb10ddd328f
Neha Patel <npate685@my.centennialcollege.ca>
2025-04-13 23:43:21 -0400

    Removed events logic from community-app; now handled by own microfrontend

--- client/community-app/src/App.jsx
+++ client/community-app/src/App.jsx
@@ -5,7 +5,6 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { communityClient } from "./apolloClients";
 import CommunityHub from "./pages/CommunityHub";
 import NewsPage from "./pages/NewsPage";
-import EventsPage from "./pages/EventsPage";
 import DiscussionsPage from "./pages/DiscussionPage";
 import HelpRequestsPage from "./pages/HelpRequestsPage";
 import BusinessPage from "./pages/BusinessPage";
@@ -25,7 +24,6 @@ function App() {
         <Routes>
           <Route path="/" element={<CommunityHub />} />
           <Route path="/news" element={<NewsPage />} />
-          <Route path="/events" element={<EventsPage />} />
           <Route path="/business" element={<BusinessPage />} />
           <Route path="/discussions" element={<DiscussionsPage />} />
           <Route path="/help" element={<HelpRequestsPage />} />
